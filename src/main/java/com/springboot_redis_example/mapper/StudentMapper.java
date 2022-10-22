17026773bada2aab8e1b540384c5c06e9064a9ad
caokuang <caokuang.cool@163.com>
2022-10-22 09:03:17 +0800

    My work

---
+++ src/main/java/com/springboot_redis_example/mapper/StudentMapper.java
@@ -0,0 +1,45 @@
+package com.springboot_redis_example.mapper;
+
+import com.springboot_redis_example.entity.Student;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+/**
+ * @Author: caokuang
+ * @Date: 2022/10/21 22:44
+ * @Version 1.0
+ */
+@Mapper
+public interface StudentMapper {
+    /**
+     * 查询用户管理信息
+     */
+    public Student selectStudentById(Integer id);
+
+    /**
+     * 新增用户管理
+     *
+     */
+    public int insertStudent(Student student);
+
+    /**
+     * 修改用户管理
+     *
+     */
+    public int updateStudent(Student student);
+
+    /**
+     * 删除用户管理
+     *
+     */
+    public int deleteStudentById(Integer id);
+
+    /**
+     * 分页查询用户
+     * @return
+     */
+    List<Student> selectPage();
+
+}
---
+++ src/main/java/com/springboot_redis_example/service/StudentImpl.java
@@ -0,0 +1,90 @@
+package com.springboot_redis_example.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.springboot_redis_example.entity.Student;
+import com.springboot_redis_example.mapper.StudentMapper;
+import com.springboot_redis_example.util.PageRequest;
+import com.springboot_redis_example.util.PageResult;
+import com.springboot_redis_example.util.PageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @Author: caokuang
+ * @Date: 2022/10/21 22:44
+ * @Version 1.0
+ */
+@Service("StudentService")
+public class StudentImpl implements StudentService{
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    /**
+     * 查询学生信息
+     * @param id
+     * @return
+     */
+    @Override
+    @Cacheable(value = "user",key = "#id")
+    public Student selectStudentById(Integer id) {
+        return studentMapper.selectStudentById(id);
+    }
+
+    /**
+     * 新增学生信息
+     * @param student
+     * @return
+     */
+    @Override
+    @Transactional
+    public int insertStudent(Student student) {
+        return studentMapper.insertStudent(student);
+    }
+
+    /**
+     * 更新用户信息
+     * @param student
+     * @return
+     */
+    @Override
+    @Transactional
+    public int updateStudent(Student student) {
+        return studentMapper.updateStudent(student);
+    }
+
+    /**
+     * 删除用户信息
+     * @param id
+     * @return
+     */
+    @Override
+    @Transactional
+    public int deleteStudent(Integer id) {
+        return studentMapper.deleteStudentById(id);
+    }
+
+    @Override
+    public PageResult findPage(PageRequest pageRequest) {
+        return PageUtils.getPageResult(pageRequest, getPageInfo(pageRequest));
+    }
+
+    /**
+     * 调用分页插件完成分页
+     * @param pageQuery
+     * @return
+     */
+    private PageInfo<Student> getPageInfo(PageRequest pageRequest) {
+        int pageNum = pageRequest.getPageNum();
+        int pageSize = pageRequest.getPageSize();
+        PageHelper.startPage(pageNum, pageSize);
+        List<Student> sysUser = studentMapper.selectPage();
+        return new PageInfo<Student>(sysUser);
+    }
+
+}
